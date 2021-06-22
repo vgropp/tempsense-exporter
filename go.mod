4ba8e7c436bf74ade2947e7c2d5ea8e211e83006
Volker Gropp <volker@gropp.org>
2021-06-23 00:48:11 +0200

    chore: cleanup code and use 16byte struct

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/vgropp/tempsense-exporter
+
+go 1.16
+
+require (
+	github.com/karalabe/hid v1.0.0
+)
--- src/cli/tempsense.go
+++ src/cli/tempsense.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"github.com/karalabe/hid"
 	"time"
 )
 
+const BufLen = 16
+
+type Data struct {
+	SensorCount    byte
+	SensorsCurrent byte
+	Power          byte
+	_              byte
+	Temp           uint16
+	_              uint16
+	SensorId       [8]byte
+}
+
 func main() {
 	devices := hid.Enumerate(0x16c0, 0x0480)
 	if len(devices) == 0 {
-		panic("No temperature sensor found!")
+		panic("no temperature sensor found!")
 	}
 	for i, device := range devices {
-		fmt.Printf("device found %v: %s => %s\n", i + 1, device.Manufacturer, device.Product)
+		fmt.Printf("device found %v: %s[%s]\n", i+1, device.Manufacturer, device.Product)
 	}
 	for {
 		for i, device := range devices {
 			readDevice(i, device, devices)
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
@@ -36,19 +50,53 @@ func readDevice(i int, device hid.DeviceInfo, devices []hid.DeviceInfo) {
 			fmt.Printf("unable to close device %v: %v\n", devNum, err)
 		}
 	}()
-	buf := make([]byte, 64)
-	read, err := open.Read(buf)
+	for {
+		curSensor, maxSensors := readNextSensor(open, devNum, len(devices))
+		if curSensor >= maxSensors {
+			break
+		}
+	}
+}
+
+func readNextSensor(device *hid.Device, devNum int, devCount int) (byte, byte) {
+	data, err := readSensor(device)
 	if err != nil {
-		fmt.Printf("could not read from device %v\n", devNum)
-		return
+		fmt.Printf("error reading device %v: %v\n", devNum, err)
+		return 0, 0
+	}
+	fmt.Printf("%v: device %v/%v, sensor %v/%v (%v): %.1f°\n",
+		time.Now().Format(time.Stamp),
+		devNum, devCount,
+		data.SensorsCurrent, data.SensorCount,
+		hex.EncodeToString(data.SensorId[:]),
+		float32(data.Temp)/10)
+	return data.SensorsCurrent, data.SensorCount
+}
+
+func readSensor(device *hid.Device) (*Data, error) {
+	buf := make([]byte, BufLen)
+	read, err := device.Read(buf)
+	if err != nil {
+		return nil, fmt.Errorf("could not read from device: %v", err)
 	}
 	if read < 0 {
-		fmt.Printf("read nothing from device %v\n", devNum)
-		return
+		return nil, fmt.Errorf("read nothing from device")
 	}
-	if read == 64 {
-		fmt.Printf("device %v/%v, sensors %v/%v: %.1f\n", devNum, len(devices), buf[1], buf[0], float32(binary.LittleEndian.Uint16(buf[4:]))/10)
-	} else {
-		fmt.Printf("device %v/%v: read %v bytes", devNum, len(devices), read)
+	if read == BufLen {
+		data, err := decode(buf)
+		if err != nil {
+			return nil, fmt.Errorf("unable to decode %v", err)
+		}
+		return data, nil
 	}
+	return nil, fmt.Errorf("read only %v bytes", read)
+}
+
+func decode(b []byte) (*Data, error) {
+	buf := bytes.NewBuffer(b)
+	data := &Data{}
+	if err := binary.Read(buf, binary.LittleEndian, data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
